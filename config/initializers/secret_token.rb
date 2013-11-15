89aa96f32116d0e249ee675b0ba171f16a7b940f
soon <bozzi.loic@gmail.com>
2013-11-15 01:12:45 -0800

    BleachBy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bleachby::Application.config.secret_token = '8a563367b6947425cb4b01990210c92a58aefc9ca0d71b8d9fecb784c9ab354162ce783d6f22f7e9e7be0a98a25ae2b2557199c1364247a21fac2dadb2c88d3f'
